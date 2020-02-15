a59fcf889421ff2deac0e0cf899a39e40907fb33
eureka_china <1767778344@qq.com>
2020-02-15 19:52:35 +0800

    将配置类移到构造函数

--- flink-base/src/main/scala/com/wufuqiang/flink/base/source/KafkaSource.scala
+++ flink-base/src/main/scala/com/wufuqiang/flink/base/source/KafkaSource.scala
@@ -13,17 +13,16 @@ class KafkaSource(serversParam:String,groupIdParam:String,topicParam:String) {
   var servers = serversParam
   var topic = topicParam
   var groupId = groupIdParam
-  var kafkaProperties:Properties = _
+  var kafkaPros:Properties = new Properties()
+
+  kafkaPros.setProperty("bootstrap.servers",servers)
+  kafkaPros.setProperty("group.id",groupId)
+  kafkaPros.setProperty("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer")
+  kafkaPros.setProperty("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer")
+  kafkaPros.setProperty("auto.offset.reset","latest")
 
   def getSource(): FlinkKafkaConsumer010[String] ={
-    val kafkaPros = new Properties()
-    kafkaPros.setProperty("bootstrap.servers",servers)
-    kafkaPros.setProperty("group.id",groupId)
-    kafkaPros.setProperty("key.deserializer","org.apache.kafka.common.serialization.StringDeserializer")
-    kafkaPros.setProperty("value.deserializer","org.apache.kafka.common.serialization.StringDeserializer")
-    kafkaPros.setProperty("auto.offset.reset","latest")
-    this.kafkaProperties = kafkaPros
-    new FlinkKafkaConsumer010[String](topic,new SimpleStringSchema(),kafkaPros)
+    new FlinkKafkaConsumer010[String](topic,new SimpleStringSchema(),this.kafkaPros)
   }
 
   def getSink():FlinkKafkaProducer010[String]={
